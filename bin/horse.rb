c7dab50e80c8c2038d2a9d5beebc4f5731c8d254
Yorick Peterse <yorickpeterse@gmail.com>
2014-09-02 00:24:51 +0200

    Allow generating of multiple blurbs at once.

--- bin/horse.rb
+++ bin/horse.rb
@@ -2,5 +2,8 @@ require 'marky_markov'
 
 dict_path = File.expand_path('../../model/default', __FILE__)
 dict      = MarkyMarkov::Dictionary.new(dict_path)
+amount    = ARGV[0] ? ARGV[0].to_i : 1
 
-puts dict.generate_1_sentences
+amount.times do
+  puts dict.generate_1_sentences
+end
