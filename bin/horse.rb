7e48c0c91c9d5fe02b50672eaeb9c7bcc70b7b48
Yorick Peterse <yorickpeterse@gmail.com>
2014-09-01 23:43:47 +0200

    Generate 20 words instead of a single sentence.
    Often the single sentences are simply too short to be even remotely funny.

--- bin/horse.rb
+++ bin/horse.rb
@@ -3,4 +3,4 @@ require 'marky_markov'
 dict_path = File.expand_path('../../model/default', __FILE__)
 dict      = MarkyMarkov::Dictionary.new(dict_path)
 
-puts dict.generate_1_sentence
+puts dict.generate_20_words
